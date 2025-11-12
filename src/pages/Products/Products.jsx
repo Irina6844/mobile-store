27b231ed5d31a60b7052fd1006f14ea4101901ae
Irina6844 <irinasargsyan3@gmail.com>
2025-11-12 14:00:18 +0400

    using useRef and scrolling

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,13 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import { Box } from "@mui/material";
 import { productContext } from "../../app/provider/provider";
 
+import './Products.css'
+
 const Products = () => {
-  
-const { products, addToCart } = useContext(productContext)
+  const { products, addToCart } = useContext(productContext);
+
+  const Line1Ref = useRef(null);
+  const Line2Ref = useRef(null);
+  const Line3Ref = useRef(null);
+
+  const scrolling = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <Box>
+      <Box sx={{ display: "flex", gap: "30px" }}>
+        <button onClick={() => scrolling(Line1Ref)} className="lineBtn">Line 1</button>
+        <button onClick={() => scrolling(Line2Ref)} className="lineBtn">Line 2</button>
+        <button onClick={() => scrolling(Line3Ref)} className="lineBtn">Line 3</button>
+      </Box>
       <Box
         sx={{
           display: "flex",
